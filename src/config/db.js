f6d5058e12dcecd188546afa55923b720ad56c13
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2020-09-07 18:41:06 -0300

    .

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 
 module.exports = new Pool({
     user: 'postgres',
-    password: 'postgres',
+    password: '12345',
     host: 'localhost',
     port: 5432,
     database: 'launchstoredb'
