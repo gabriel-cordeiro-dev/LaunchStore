60c36520673add22e92c6d7185ccd6a97993c291
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2020-10-26 14:05:16 -0300

    configurando e estilizando pagina create
    inserindo estilizacao na pagina, forms,inputs e button

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const routes = express.Router()
-const ProductController = require('ProductController')
+const ProductController = require('./app/controllers/ProductController')
 
 
 routes.get('/', function (req, res){
