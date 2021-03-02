1f1f1d34930e2b67cefd36a1b1e5f750df07e732
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2021-03-02 11:45:36 -0300

    Incluindo Categorias
    criando e trazendo do banco as categorias

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,5 +1,24 @@
+const Category = require('../models/Category');
+
 module.exports = {
     create(req, res){
+        //pegar categorias
+        Category.all()
+        .then(function(results){
+
+            const categories = results.rows
+
+
+            return res.render('products/create.njk', { categories })
+
+        }).catch(function(err){
+            throw new Error(err)
+        })
+
+
         return res.render('products/create.njk')
+    },
+    post(req, res){
+        //salvar
     }
 }
