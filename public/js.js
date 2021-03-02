725e9f7022875fb0d1c87993d58f4526b69a985d
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2021-03-02 16:46:33 -0300

    keep going
    arrumando código, criando mascaras e inserindo opção de fotos, assim como seu tratamento

--- public/js.js
+++ public/js.js
@@ -1,6 +1,6 @@
 const Mask = {
     apply(input, func){
-        setTimeout(function() {
+        setTimeout(() => {
             input.value = Mask[func](input.value)
         },1)
     },
@@ -12,4 +12,54 @@ const Mask = {
             currency: 'BRL',
         }).format(value/100)
     },
+}
+
+
+const PhotosUpload = {
+    preview: document.querySelector('#photos-preview'),
+    uploadLimit: 6,
+    handleFileInput(event){
+        const { files: fileList } = event.target
+        
+        if(PhotosUpload.hasLimit(event)) return
+
+        Array.from(fileList).forEach(file => {
+            const reader = new FileReader()
+
+
+            reader.onload = () =>{
+                const image = new Image()
+                image.src = String(reader.result)
+
+                const div = PhotosUpload.getContainer(image)
+
+                PhotosUpload.preview.appendChild(div)
+            }
+
+            reader.readAsDataURL(file)
+        })
+
+    },
+    hasLimit(event){
+        const { uploadLimit } = PhotosUpload
+
+        if (fileList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} fotos`)
+            event.preventDefault()
+            return true
+        }
+
+        return false
+    },
+    getContainer(){
+        const div = document.createElement('div')
+        div.classList.add('photo')
+
+        div.onclick = () => alert('cliquei')
+
+        div.appendChild(image)
+
+        return div
+    },
+
 }
