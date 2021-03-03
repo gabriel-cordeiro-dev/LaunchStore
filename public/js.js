e8647a7902667d646660c75b9fe2d0d048b6ae09
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2021-03-02 22:27:30 -0300

    configurando upload images
    feito a config do upload de imagens, assim como seu tratamento

--- public/js.js
+++ public/js.js
@@ -16,14 +16,22 @@ const Mask = {
 
 
 const PhotosUpload = {
+    input: "",
     preview: document.querySelector('#photos-preview'),
     uploadLimit: 6,
+    files: [],
     handleFileInput(event){
         const { files: fileList } = event.target
+        PhotosUpload.input = event.target
         
         if(PhotosUpload.hasLimit(event)) return
 
         Array.from(fileList).forEach(file => {
+
+            PhotosUpload.files.push(file)
+
+
+
             const reader = new FileReader()
 
 
@@ -39,9 +47,12 @@ const PhotosUpload = {
             reader.readAsDataURL(file)
         })
 
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
     },
     hasLimit(event){
-        const { uploadLimit } = PhotosUpload
+        const { uploadLimit, input, preview } = PhotosUpload
+        const { files: fileList } = input
 
         if (fileList.length > uploadLimit) {
             alert(`Envie no máximo ${uploadLimit} fotos`)
@@ -49,17 +60,58 @@ const PhotosUpload = {
             return true
         }
 
+        const photosDiv = []
+        preview.childNodes.forEach(item => {
+            if (item.classList && item.classList.value == "photo") {
+                photosDiv.push(item)
+            }
+        })
+
+        const totalPhotos = fileList.length + photosDiv.length
+        if (totalPhotos > uploadLimit) {
+            alert("Você atingiu o limite de fotos permitido")
+            event.preventDefault()
+            return true
+        }
+
         return false
     },
-    getContainer(){
+    getAllFiles(){
+        const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+        PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+
+        return dataTransfer.files
+        
+    },
+    getContainer(image){
         const div = document.createElement('div')
         div.classList.add('photo')
 
-        div.onclick = () => alert('cliquei')
+        div.onclick = PhotosUpload.removePhoto
 
         div.appendChild(image)
 
+        div.appendChild(PhotosUpload.getRemoveButton())
+
         return div
     },
+    getRemoveButton(){
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = "close"
+        return button
+
+    },
+    removePhoto(event) {
+        const photoDiv = event.target.parentNode
+        const photosArray = Array.from(PhotosUpload.preview.children)
+        const index = photosArray.indexOf(photoDiv)
+
+        PhotosUpload.files.splice(index, 1)
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+        photoDiv.remove()
+    }
 
 }
